0dfb6a52ecdb3a4e9045323a21ac5d57efebf096
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-06-10 12:45:54 +0200

    setting up the timer intervall

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,6 +12,8 @@ const loginEl = document.querySelector("#login-form")
 
 const titleEl = document.querySelector("#title-wrapper")
 
+const timerEl = document.querySelector("#timer")
+
 const gameEl = document.querySelector("#second-wrapper")
 
 gameEl.style.display = 'none'
@@ -22,11 +24,18 @@ waitingMsgEl.style.display = 'none'
 
 let rounds = 0;
 
-let timeStamp = ""
+let timeStamp = Date.now()
 
 document.querySelector('#login-form').addEventListener('submit', e => {
 
     e.preventDefault();
+
+    if (nickEl.value=="") {
+
+        alert('YOU NEED TO ENTER A COOL NAME');
+
+        return
+    }
 
     titleEl.style.display = "none";
 
@@ -46,13 +55,31 @@ socket.on('start', users=>{
         waitingMsgEl.style.display = 'none';
         usersEl.innerHTML = `${users[0]} AGAINST ${users[1]}`
 
+
+        let time =  new Date('dec 31, 2020 00:00:00')
+
+        let n = 0
+
+        setInterval(function() {
+
+            time.setSeconds(n) 
+
+            timerEl.innerHTML = time.getSeconds()
+
+            n += 1
+
+        }, 1000)
+
+
         rounds +=1
 
         if (rounds==10) {
 
             alert('End of the game')
-            
+
         }
+
+        rounds= 0;
 
 })
 // socket.on('chatmsg', dataobject => {
