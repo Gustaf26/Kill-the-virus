2b36bca45344cb290f4f0072832c116299c1af77
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2020-06-10 08:48:33 +0200

    setting up the app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Serve static files
+app.use(express.static( 'public'))
+
+
+
+module.exports = app;
